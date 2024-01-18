23e23f88284ea8ed194afcfaffc991b15abcaf69
Clément <95907390+clementpnn@users.noreply.github.com>
2024-01-18 21:49:10 +0100

    add future routes

--- server/internal/app/router/router.go
+++ server/internal/app/router/router.go
@@ -11,6 +11,10 @@ import (
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api", logger.New())
 	api.Get("/", handler.Hello)
+	// ? Lister tous les restaurants disponibles dans le food-court.
+	api.Get("/restaurant") // TODO: créé handler
+	// ? Voir le menu d’un restaurant.
+	api.Get("/restaurant/:id/menu") // TODO: créé handler
 
 	auth := api.Group("/auth")
 
@@ -26,6 +30,39 @@ func SetupRoutes(app *fiber.App) {
 
 	client := api.Group("/client", middleware.AuthMiddleware("client"))
 	client.Get("/:id", handler.GetClient)
+	// ? Lister les commandes d'un client.
+	client.Get("/order") // TODO: créé handler
+	// ? Créer une commande.
+	client.Post("/order") // TODO: créé handler
+	// ? Voir une commande.
+	client.Get("/order/:id") // TODO: créé handler
+	// ? Modifier une commande.
+	client.Put("/order/:id") // TODO: créé handler
+	// ? Supprimer une commande.
+	client.Delete("/order/:id") // TODO: créé handler
+
+	admin := api.Group("/admin", middleware.AuthMiddleware("admin"))
+	// ? Lister tous les restaurants disponibles dans le food-court.
+	admin.Get("/restaurant") // TODO: créé handler
+	// ? Créer un restaurant.
+	admin.Post("/restaurant") // TODO: créé handler
+	// ? Modifier un restaurant.
+	admin.Put("/restaurant/:id") // TODO: créé handler
+	// ? Supprimer un restaurant.
+	admin.Delete("/restaurant/:id") // TODO: créé handler
+
+	restaurant := api.Group("/restaurant", middleware.AuthMiddleware("restaurant"))
+	restaurant.Get("/:id/", handler.GetRestaurant)
+	// ? Lister toutes les commandes pour un restaurant.
+	restaurant.Get("/:id/order") // TODO: créé handler (SSE ou WebSockets)
+	// ? Mettre à jour le statut d'une commande.
+	restaurant.Put("/:id/order/:orderId") // TODO: créé handler
+	// ? Ajouter des items au menu.
+	restaurant.Post("/:id/menu") // TODO: créé handler
+	// ? Modifier un item du menu.
+	restaurant.Put("/:id/menu/:itemId") // TODO: créé handler
+	// ? Supprimer un item du menu.
+	restaurant.Delete("/:id/menu/:itemId") // TODO: créé handler
 
 	app.Use(func(c *fiber.Ctx) error {
 		return c.SendStatus(404)
