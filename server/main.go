b55ba421aaac13470cedf55dbf29b46e0d6ee9dc
Clément <95907390+clementpnn@users.noreply.github.com>
2024-01-18 15:25:11 +0100

    add fiber

--- server/main.go
+++ server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"go-manger/internal/app/router"
+	"go-manger/internal/infrastructure/database"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	database.Database()
+
+	router.SetupRoutes(app)
+
+	log.Fatal(app.Listen(":3000"))
 }
