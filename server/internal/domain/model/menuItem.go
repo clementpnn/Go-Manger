2e83df4f874475c68a8b2c2a498f04525356d8a4
Clément <95907390+clementpnn@users.noreply.github.com>
2024-01-18 21:15:10 +0100

    update meneItem for available and type

--- server/internal/domain/model/menuItem.go
+++ server/internal/domain/model/menuItem.go
@@ -4,11 +4,22 @@ import (
 	"gorm.io/gorm"
 )
 
+type MenuItemType string
+
+const (
+	MenuItemStarter MenuItemType = "starter"
+	MenuItemDish    MenuItemType = "dish"
+	MenuItemDessert MenuItemType = "dessert"
+	MenuItemDrink   MenuItemType = "drink"
+)
+
 type MenuItem struct {
 	gorm.Model
-	Name         string  `gorm:"not null" json:"name"`
-	Description  string  `gorm:"not null" json:"description"`
-	Image        string  `gorm:"not null" json:"image"`
-	Price        float64 `gorm:"not null" json:"price"`
-	RestaurantID uint    `gorm:"not null" json:"restaurantId"`
+	Name         string       `gorm:"not null" json:"name"`
+	Description  string       `gorm:"not null" json:"description"`
+	Image        string       `gorm:"not null" json:"image"`
+	Available    bool         `gorm:"not null" json:"available"`
+	Type         MenuItemType `gorm:"type:varchar(20);check:status IN ('starter', 'dish', 'dessert', 'drink')" json:"type"`
+	Price        float64      `gorm:"not null" json:"price"`
+	RestaurantID uint         `gorm:"not null" json:"restaurantId"`
 }
