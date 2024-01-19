023749e9364c68143dcb90fa47716a8978474c5c
Clément <95907390+clementpnn@users.noreply.github.com>
2024-01-19 16:02:18 +0100

    fix type

--- server/internal/domain/service/getIdFromJWT.go
+++ server/internal/domain/service/getIdFromJWT.go
@@ -5,11 +5,11 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-func GetUserIDFromJWT(c *fiber.Ctx) (int, error) {
+func GetUserIDFromJWT(c *fiber.Ctx) (float64, error) {
 	user := c.Locals("user")
 	token := user.(*jwt.Token)
 	claims := token.Claims.(jwt.MapClaims)
-	userID := claims["user_id"].(int)
+	userID := claims["user_id"].(float64)
 
 	return userID, nil
 }
