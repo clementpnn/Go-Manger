fb1dc0978a4ab08b32e888574ccbd1bf4859bba3
Clément <95907390+clementpnn@users.noreply.github.com>
2024-01-21 12:17:12 +0100

    cors bug

--- web/src/services/utils/instance.ts
+++ web/src/services/utils/instance.ts
@@ -1,16 +1,16 @@
-import useJWT from "@/hooks/useJWT"
+// import useJWT from "@/hooks/useJWT"
 import ky from "ky"
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
-const { jwt } = useJWT()
+// const { jwt } = useJWT()
 
 export const kyInstancePublic = ky.create({
   prefixUrl: "http://127.0.0.1:3000/api",
 })
 
-export const kyInstancePrivate = ky.create({
-  prefixUrl: "http://127.0.0.1:3000/api",
-  Headers: {
-    Authorization: `Bearer ${jwt}`
-  }
-})
+// export const kyInstancePrivate = ky.create({
+//   prefixUrl: "http://127.0.0.1:3000/api",
+//   Headers: {
+//     Authorization: `Bearer ${jwt}`
+//   }
+// })
