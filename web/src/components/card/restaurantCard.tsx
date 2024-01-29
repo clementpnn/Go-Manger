e1c1424968429859c613065dca11e20b0e4f5bb2
vitolinho <93949338+vitolinho@users.noreply.github.com>
2024-01-29 18:03:16 +0100

    remove useless link in restaurant card

--- web/src/components/card/restaurantCard.tsx
+++ web/src/components/card/restaurantCard.tsx
@@ -1,5 +1,3 @@
-import { Link } from "@tanstack/react-router"
-
 export interface RestaurantCardProperties {
   restaurantName: string,
   description: string,
@@ -8,16 +6,13 @@ export interface RestaurantCardProperties {
 
 const RestaurantCard: React.FC<RestaurantCardProperties> = ({ restaurantName, description, image }) => {
   return (
-    // TODO ajouter la logique pour récupérer le chemin vers restaurant/$id
-    <Link to="/">
-      <div className="relative h-[19.063rem] w-[19.063rem] flex flex-col justify-end">
-        <img src={image} alt={image} className="h-[19.063rem] w-[19.063rem] object-cover rounded-md brightness-50"/>
-        <div className="absolute flex flex-col px-2 py-2">
-          <p className="header-3 text-neutral-0 z-40">{restaurantName}</p>
-          <p className="body text-neutral-1 z-40">{description}</p>
-        </div>
+    <div className="relative h-[19.063rem] w-[19.063rem] flex flex-col justify-end">
+      <img src={image} alt={image} className="h-[19.063rem] w-[19.063rem] object-cover rounded-md brightness-50"/>
+      <div className="absolute flex flex-col px-2 py-2">
+        <p className="header-3 text-neutral-0 z-40">{restaurantName}</p>
+        <p className="body text-neutral-1 z-40">{description}</p>
       </div>
-    </Link>
+    </div>
   )
 }
 
