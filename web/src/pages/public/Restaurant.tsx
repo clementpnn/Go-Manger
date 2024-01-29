7c2b47c95e21c3d7f5da8e1d297db5c11caa5d14
vitolinho <93949338+vitolinho@users.noreply.github.com>
2024-01-29 23:54:19 +0100

    added style to restaurant id page

--- web/src/pages/public/Restaurant.tsx
+++ web/src/pages/public/Restaurant.tsx
@@ -1,5 +1,7 @@
-import { H4 } from "@/components/typography/h4";
-import { H3 } from "@/components/typography/h3";
+// import { H4 } from "@/components/typography/h4";
+// import { H3 } from "@/components/typography/h3";
+// import { P } from "@/components/typography/p";
+import NavbarAdmin from "@/components/navbar/navbarAdmin";
 import { P } from "@/components/typography/p";
 import { RestaurantInfoPublicService } from "@/services/public";
 import { useQuery } from "@tanstack/react-query";
@@ -21,19 +23,39 @@ export default function Restaurant() {
   }
 
   return (
-    <div className="flex flex-col gap-4">
-      <H3>{data.data.name}</H3>
-      <H4>{data.data.description}</H4>
-      <P>{data.data.image}</P>
-      {data.data.menuItems.map((item: any) => (
-        <div key={item.id}>
-          <P>{item.name}</P>
-          <P>{item.description}</P>
-          <P>{item.price}</P>
-          <P>{item.available}</P>
-          <P>{item.type}</P>
+    <div className="flex flex-col gap-y-[3.75rem]">
+      <NavbarAdmin />
+      <div className="flex flex-row justify-between items-center px-20">
+        <div className="flex flex-col gap-x-4">
+          <p className="header-2 text-neutral-3">{data.data.name}</p>
+          <p className="body text-neutral-2">{data.data.description}</p>
         </div>
-      ))}
+        <img src={data.data.image} alt={`image of ${data.data.image}`} className="w-[25.75rem] h-[13.25rem] rounded-md"/>
+      </div>
+      <div className="flex flex-col gap-y-10 px-20">
+        <p className="header-3 text-neutral-3">Menu</p>
+        <div className="flex flex-wrap justify-between gap-20">
+          {data.data.menuItems.map((item: any) => (
+            <ul key={item.id}>
+              <li className="list-disc body text-neutral-2">
+                {item.name}
+              </li>
+              <li className="list-disc body text-neutral-2">
+                {item.description}
+              </li>
+              <li className="list-disc body text-neutral-2">
+                {item.price}
+              </li>
+              <li className="list-disc body text-neutral-2">
+                {item.available === true ? "available" : "not available"}
+              </li>
+              <li className="list-disc body text-neutral-2">
+                {item.type}
+              </li>
+            </ul>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
