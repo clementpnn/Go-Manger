946818d9107a237d473255f786d2f84b3356366f
Victor <victor.huang@shadow.tech>
2024-02-10 22:55:06 +0100

    Add Delete Restaurant Service

--- web/src/services/restaurant.ts
+++ web/src/services/restaurant.ts
@@ -31,3 +31,7 @@ export function DeleteRestaurantMenuService(id: number): Promise<ApiRequest> {
 export function UpdateRestaurantMenuService({ name, description, type, price, id }: { name: string, description: string, type: MenuItemType, price : number, id: string }): Promise<ApiRequest> {
   return KyInstancePrivate.put(`restaurant/menu/${Number(id)}`, { json: { name, description, type, price } }).json()
 }
+
+export function DeleteRestaurantService(): Promise<ApiRequest> {
+  return KyInstancePrivate.delete("restaurant/me/").json()
+}
