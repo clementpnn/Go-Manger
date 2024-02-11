927af1659348fa12b2bf0b2a18642cb3182d4a4f
vitolinho <93949338+vitolinho@users.noreply.github.com>
2024-02-11 22:24:29 +0100

    changed update

--- web/src/types/update.ts
+++ web/src/types/update.ts
@@ -3,65 +3,32 @@ import { z } from "zod";
 export const UpdateAdminProfile = z.object({
   email: z
     .string()
-    .email()
     .max(255, { message: "Email cannot be longer than 255 characters" }),
-  name: z.string().min(4, { message: "Username cannot be empty" }).max(255, { message: "Username cannot be longer than 255 characters" }),
+  name: z.string().max(255, { message: "Username cannot be longer than 255 characters" }),
   password: z
     .string()
     .max(180, { message: "Password cannot be longer than 180 characters" })
-    .regex(/[a-z]/, {
-      message: "Password must contain at least one lowercase letter",
-    })
-    .regex(/[A-Z]/, {
-      message: "Password must contain at least one uppercase letter",
-    })
-    .regex(/[0-9]/, { message: "Password must contain at least one number" })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: "Password must contain at least one special character",
-    }),
 });
 
 export const UpdateClientProfile = z
   .object({
     email: z
       .string()
-      .email()
       .max(255, { message: "Email cannot be longer than 255 characters" }),
-    name: z.string().min(4, { message: "Username cannot be empty" }).max(25, { message: "Username cannot be longer than 25 characters" }),
+    name: z.string().max(25, { message: "Username cannot be longer than 25 characters" }),
     password: z
       .string()
       .max(180, { message: "Password cannot be longer than 180 characters" })
-      .regex(/[a-z]/, {
-        message: "Password must contain at least one lowercase letter",
-      })
-      .regex(/[A-Z]/, {
-        message: "Password must contain at least one uppercase letter",
-      })
-      .regex(/[0-9]/, { message: "Password must contain at least one number" })
-      .regex(/[^a-zA-Z0-9]/, {
-        message: "Password must contain at least one special character",
-      }),
   })
 
 export const UpdateRestaurantProfile = z.object({
   email: z
     .string()
-    .email()
     .max(255, { message: "Email cannot be longer than 255 characters" }),
-  name: z.string().min(4, { message: "Username cannot be empty" }).max(255, { message: "Username cannot be longer than 255 characters" }),
+  name: z.string().max(255, { message: "Username cannot be longer than 255 characters" }),
   password: z
     .string()
-    .max(180, { message: "Password cannot be longer than 180 characters" })
-    .regex(/[a-z]/, {
-      message: "Password must contain at least one lowercase letter",
-    })
-    .regex(/[A-Z]/, {
-      message: "Password must contain at least one uppercase letter",
-    })
-    .regex(/[0-9]/, { message: "Password must contain at least one number" })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: "Password must contain at least one special character",
-    }),
+    .max(180, { message: "Password cannot be longer than 180 characters" }),
   description: z
     .string()
     .max(255, { message: "Description cannot be longer than 255 characters" }),
