146554de2bc4be1d68157925a72a635d89676094
vitolinho <93949338+vitolinho@users.noreply.github.com>
2024-02-11 21:52:45 +0100

    fix delete client button

--- web/src/components/button/DeleteClientButton.tsx
+++ web/src/components/button/DeleteClientButton.tsx
@@ -5,12 +5,11 @@ import { DeleteClientService } from "@/services/client"
 
 export default function DeleteClientButton() {
   const { mutate } = useMutation({ mutationFn: DeleteClientService })
-  const navigate = useNavigate()
+  const navigate = useNavigate({ from: "/client/profile" })
   const handleDeleteClientClick = () => {
     mutate()
     localStorage.removeItem("jwtToken")
     navigate({ to: "/signin" })
-    window.location.reload()
   }
   return (
     <Button onClick={handleDeleteClientClick} variant={"destructive"}>Supprimer</Button>
